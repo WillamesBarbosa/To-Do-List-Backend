18840e50dbd14c442a598f505eefa36e7b971816
WillamesBarbosa <willamessilva1999@gmail.com>
2025-01-07 19:56:09 -0300

    feat(taskController): function create implemented

--- src/app/controllers/taskController.js
+++ src/app/controllers/taskController.js
@@ -1,6 +1,30 @@
+const verifyParams = require("../utils/verifyParams");
+
+const bd = [];
+
 class TaskController{
     async index(request, response){
-        response.json({message: 'Bem-vindo à API de tarefas'})
+        response.json({message: 'Bem-vindo à API de tarefas'});
+    }
+
+    async create(request, response){
+        const {title, description} = request.body;
+
+        const verifyAllParams = verifyParams(title, description);
+        console.log(verifyAllParams)
+
+        if(verifyAllParams){
+            const obj = {title: title, description: description};
+            console.log(obj)
+            bd.push(obj);
+            const json = JSON.stringify(obj)
+            console.log(json)
+            return response.json(json)
+        }
+
+        return response.json({Error: 'Titulo ou descrição vazios'})
+
+
     }
 }
 module.exports = new TaskController();
