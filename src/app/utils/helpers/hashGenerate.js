960bc97c8d80bd9acd60a5723680270736fc99b9
WillamesBarbosa <willamessilva1999@gmail.com>
2025-01-08 20:54:58 -0300

    fix(hashGenerate.js): random number added to fix the bug of always generating the same hash

--- src/app/utils/helpers/hashGenerate.js
+++ src/app/utils/helpers/hashGenerate.js
@@ -2,7 +2,7 @@ const crypto = require('crypto');
 const HashCode = require('../../../../code.hash');
 
 function hashGenerate(data) {
-    const uniqueString = `${HashCode}-${data}-${Date.now()}`;
+    const uniqueString = `${HashCode}-${data}-${Date.now()}-${Math.random()}`;
     const newHash = crypto.createHash('sha256').update(uniqueString).digest('hex');
 
     return newHash
