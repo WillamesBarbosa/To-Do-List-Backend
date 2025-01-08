ecef7fbdcee72bebcd7fbf0a68fe2d57202d4a15
WillamesBarbosa <willamessilva1999@gmail.com>
2025-01-08 20:56:42 -0300

    test(hashGenerate.test.js): the hashGenerate test  are created

---
+++ src/app/utils/helpers/hashGenerate.test.js
@@ -0,0 +1,14 @@
+const hashGenerate = require('./hashGenerate');
+
+test('Should return different hash', ()=>{
+    const hash1 = hashGenerate('data');
+    const hash2 = hashGenerate('data');
+
+    expect(hash1 === hash2).toEqual(false)
+})
+
+test('Should have length to equal 64', ()=>{
+    const hash = hashGenerate('data');
+
+    expect(hash).toHaveLength(64);
+})
