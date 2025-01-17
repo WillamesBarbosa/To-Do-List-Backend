be079b944c313d7638fc3084f0b11d26ce3065f5
WillamesBarbosa <willamessilva1999@gmail.com>
2025-01-16 22:27:49 -0300

    feat(routes.js): a new endpoint for update tasks

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,5 +5,6 @@ const TaskController = require('../app/controllers/TaskController');
 
 router.get('/task', TaskController.index);
 router.post('/task', TaskController.store);
+router.put('/task/:id', TaskController.update);
 
 module.exports = router;
