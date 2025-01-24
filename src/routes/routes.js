e96b95f3c110c10dd9c6c87997ab5c69358188b9
WillamesBarbosa <willamessilva1999@gmail.com>
2025-01-23 21:21:28 -0300

    feat(router): a new route delete is created to delete task

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,5 +6,6 @@ const TaskController = require('../app/controllers/TaskController');
 router.get('/task', TaskController.index);
 router.post('/task', TaskController.store);
 router.put('/task/:id', TaskController.update);
+router.delete('/task/:id', TaskController.delete);
 
 module.exports = router;
