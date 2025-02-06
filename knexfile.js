cf70c8aa1057eea844179b5f7eea88e17e1f1282
WillamesBarbosa <willamessilva1999@gmail.com>
2025-02-06 20:18:51 -0300

    feat(knexfile.js/config-knex.js): create archive to configure and connect to database

---
+++ knexfile.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+
+
+module.exports = {
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: ':memory:'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './src/database/migrations'
+    },
+    seeds: {
+      directory: './src/database/seeds'
+    }
+  },
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './src/database/migrations'
+    },
+    seeds: {
+      directory: './src/database/seeds'
+    }
+  }
+};
---
+++ src/database/config/config-knex.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const knexConfig = require('../../../knexfile');
+
+const enviroment = process.env.NODE_ENV
+const knexConfigured = knexConfig[enviroment]
+
+const database = require('knex')(knexConfigured);
+
+
+process.on('SIGTERM', async () => {
+    console.log('\nEncerrando o servidor...');
+    await knex.destroy();
+    server.close(() => process.exit(0));
+  });
+  
+process.on('SIGINT', async () => {
+    console.log('Fechando conexão com o banco...');
+    await knex.destroy();
+    process.exit(0);
+});
+
+module.exports = database;
