cf8bb602e53be28ee1b7da2dd417ee9136c03151
WillamesBarbosa <willamessilva1999@gmail.com>
2025-02-06 21:20:37 -0300

    refactor(isValidHash.js/isValidUUID.js): the function has been updated to adapt to the new identification functionality

--- src/app/utils/validators/isValidIdHash.js
+++
@@ -1,7 +0,0 @@
-function isValidHash(id) {
-    const hashRegex = /^[a-fA-F0-9]{64}$/;
-    return hashRegex.test(id);
-}
-
-
-module.exports = isValidHash;
---
+++ src/app/utils/validators/isValidUUID.js
@@ -0,0 +1,7 @@
+function isValidUUID(id) {
+    const uuidRegex = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/;
+    return uuidRegex.test(id);
+}
+
+
+module.exports = isValidUUID;
