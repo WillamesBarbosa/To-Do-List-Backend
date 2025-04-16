8e19e1c906da07f47786d06200cd06e7c06a4ee1
WillamesBarbosa <willamessilva1999@gmail.com>
2025-04-16 20:55:03 -0300

    feat(UserRepository): create findAll function to find all users

--- src/app/repositories/UserRepository/userRepository.js
+++ src/app/repositories/UserRepository/userRepository.js
@@ -2,6 +2,12 @@ const database = require('../../../database/config/config-knex');
 
 class UserRepository{
 
+    async findAll(){
+        const rows = await database('users').select('*');
+
+        return rows;
+    }
+
     async findById(id){
         const row = await database('users').where('id', id);
         if(row.length === 0){
@@ -22,11 +28,11 @@ class UserRepository{
 
     async create(id, name, email, password){
         const data = {
-            id, name, email, password,
+            id: id, name: name, email: email, password: password,
         }
 
-        const row = await database('users').insert(data).returning('*');
-
+        const [ row ] = await database('users').insert(data).returning('*');
+        
         return row;
     }
 }
