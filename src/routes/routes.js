dc9712f078534185e291cd3bff7966070d0e73a1
WillamesBarbosa <willamessilva1999@gmail.com>
2025-04-16 20:56:44 -0300

    feat(routes): create post /user and get /users to connect to respective controllers

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,7 @@ const Router = require('express');
 const router = Router();
 
 const TaskController = require('../app/controllers/TaskController/taskController');
+const userController = require('../app/controllers/UserController/userController');
 const httpErrorHandler = require('../middlewares/httpErrorMiddleware');
 const resolver = require('../app/utils/adapter/resolverHandlerFn');
 
@@ -10,6 +11,9 @@ router.get('/task/:id', resolver(TaskController.show));
 router.post('/task', resolver(TaskController.store));
 router.put('/task/:id', resolver(TaskController.update));
 router.delete('/task/:id', resolver(TaskController.delete));
+
+router.get('/users', resolver(userController.index));
+router.post('/user', resolver(userController.store));
 router.use(httpErrorHandler)
 
 module.exports = router;
