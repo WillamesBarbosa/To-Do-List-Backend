2d4cc89e4cb69a84dd62b1e257b0e446525f0211
WillamesBarbosa <willamessilva1999@gmail.com>
2025-04-25 20:03:48 -0300

    test(verifyParams): update to adapt to new features

--- src/app/utils/validators/verifyParams/verifyParams.test.js
+++ src/app/utils/validators/verifyParams/verifyParams.test.js
@@ -4,7 +4,7 @@ test('Should return true if title and description exist', ()=>{
     const title = 'Titulo';
     const description = 'Descrição';
 
-    const validation = verifyParams(title, description);
+    const validation = verifyParams({ title, description });
 
     expect(validation.valid).toEqual(true);
 })
@@ -13,7 +13,7 @@ test('Should return false if title is empty and message error', ()=>{
     const title = '';
     const description = 'Descrição'
 
-    const validation = verifyParams(title, description);
+    const validation = verifyParams({ title, description });
 
     expect(validation.message).toEqual({ error: 'Title is required.' })
     expect(validation.valid).toEqual(false);
@@ -24,7 +24,7 @@ test('Should return false if description is empty and message error', ()=>{
     const title = 'Titulo';
     const description = ''
 
-    const validation = verifyParams(title, description);
+    const validation = verifyParams({ title, description });
 
     expect(validation.message).toEqual({ error: 'Description is required.' })
     expect(validation.valid).toEqual(false);
@@ -36,7 +36,7 @@ test('should return false if both are empty and message error', ()=>{
     const title = '';
     const description = ''
 
-    const validation = verifyParams(title, description);
+    const validation = verifyParams({ title, description });
 
     expect(validation.message).toEqual({ error: 'Title is required.' })
     expect(validation.valid).toEqual(false);
