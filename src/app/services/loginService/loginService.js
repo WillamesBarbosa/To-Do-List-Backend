035e4f96cd2465693c25ab802daf9b603d1ea19c
WillamesBarbosa <willamessilva1999@gmail.com>
2025-05-13 15:47:35 -0300

    feat: create loginService to intermediate loginController and generateToken

---
+++ src/app/services/loginService/loginService.js
@@ -0,0 +1,16 @@
+const { findByEmail } = require("../../repositories/UserRepository/userRepository");
+const generateToken = require("../generateToken/generateToken");
+
+async function loginService(email, password){
+    console.log('chegou aqui no service')
+        const user = await findByEmail(email);
+        if(!user) return { isValid: false, message: { error: 'Email not found.'} }
+        
+        const token = await generateToken(user, password);
+        if(!token) return { isValid: false, message: { error: 'Password incorrect.'} }
+        
+        return {isValid: true, token};
+
+}
+
+module.exports = loginService;
