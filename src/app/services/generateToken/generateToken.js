ac8de8f1556648445767142da1234cbf831a7250
WillamesBarbosa <willamessilva1999@gmail.com>
2025-05-13 15:46:52 -0300

    feat: create generateToken to gerenate jwt token

---
+++ src/app/services/generateToken/generateToken.js
@@ -0,0 +1,33 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+async function generateToken(user, password){
+    const firstText = process.env.PASS_FIRSTTEXT;
+    const secondText = process.env.PASS_SECONDTEXT;
+    const toCompare = `${firstText}${password}${secondText}`;
+    const [ userObj ] = user;
+
+    const tokenSecret = process.env.TOKEN_SECRET; 
+    const tokenExpiration = process.env.TOKEN_EXPIRATION;
+
+    const passwordAuth = await bcrypt.compare(toCompare, userObj.password);
+
+    if(!passwordAuth) return false;
+    
+
+    const token = await jwt.sign(
+        {
+            id: user.id
+        }, 
+        tokenSecret,
+        {
+            expiresIn: tokenExpiration,
+        } 
+    )
+
+    return token
+
+}
+
+module.exports = generateToken;
