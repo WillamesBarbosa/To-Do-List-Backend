0dccddf8bdea107eba48a234c6e095300f718050
WillamesBarbosa <willamessilva1999@gmail.com>
2025-05-13 15:47:59 -0300

    feat: create loginController

---
+++ src/app/controllers/LoginController/loginController.js
@@ -0,0 +1,25 @@
+const loginService = require("../../services/loginService/loginService");
+const ErrorsHTTP = require("../../utils/helpers/ErrorsHTTP");
+const responsesHTTP = require("../../utils/helpers/responsesHTTPS");
+const isValidEmail = require("../../utils/validators/isValidEmail/isValidEmail");
+const verifyParams = require("../../utils/validators/verifyParams/verifyParams");
+
+class LoginController {
+    async login(request, response){
+        const { email, password } = request.body;
+        
+        const emailOrPasswordIsUndefined = verifyParams({ email, password });
+        if(!emailOrPasswordIsUndefined.valid) throw new ErrorsHTTP(emailOrPasswordIsUndefined.message, responsesHTTP.BAD_REQUEST.status);
+        
+        const emailIsValid = isValidEmail(email);
+        if(!emailIsValid.isValid) throw new ErrorsHTTP(emailIsValid.message, responsesHTTP.BAD_REQUEST.status);
+        console.log(email)
+
+        const token = await loginService(email, password);
+        if(!token.isValid) throw new ErrorsHTTP(token.message, responsesHTTP.BAD_REQUEST.status);
+
+        return response.status(200).json(token.token);
+    }
+}
+
+module.exports = new LoginController();
