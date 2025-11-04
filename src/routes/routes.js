d6347c318c9f46ae598e9d6c84c998b0095f2365
WillamesBarbosa <willamessilva1999@gmail.com>
2025-11-03 21:10:36 -0300

    chore(tests): create jest.integration.config and jest.unit.config and create commands in package.json

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,9 +6,13 @@ const UserController = require('../app/controllers/UserController/userController
 const httpErrorHandler = require('../middlewares/httpErrorMiddleware');
 const resolver = require('../app/utils/adapter/resolverHandlerFn');
 const LoginController = require('../app/controllers/LoginController/loginController');
+// const authenticationMiddleware = require('../middlewares/authenticationMiddleware/authenticationMiddleware');
 
 router.post('/login', resolver(LoginController.login))
 
+router.post('/user', resolver(UserController.store));
+
+// router.use(authenticationMiddleware);
 router.get('/tasks', resolver(TaskController.index));
 router.get('/task/:id', resolver(TaskController.show));
 router.post('/task', resolver(TaskController.store));
@@ -16,9 +20,11 @@ router.put('/task/:id', resolver(TaskController.update));
 router.delete('/task/:id', resolver(TaskController.delete));
 
 router.get('/users', resolver(UserController.index));
-router.post('/user', resolver(UserController.store));
 router.put('/user/:id', resolver(UserController.update));
 router.delete('/user/:id', resolver(UserController.delete))
+
+router.get('/user/:id', resolver(UserController.show));
+
 router.use(httpErrorHandler)
 
 module.exports = router;
