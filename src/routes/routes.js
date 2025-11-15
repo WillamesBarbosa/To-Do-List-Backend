856c08f432988b74c7bd0edde9d5a7605501607d
WillamesBarbosa <willamessilva1999@gmail.com>
2025-11-14 21:00:04 -0300

    refactor(routes): Show, update, and delete routes should be protected by authentication middleware.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -10,20 +10,23 @@ const authenticationMiddleware = require('../middlewares/authenticationMiddlewar
 
 router.post('/login', resolver(LoginController.login))
 
+router.get('/users', resolver(UserController.index));
 router.post('/user', resolver(UserController.store));
 
 router.get('/tasks', resolver(TaskController.index));
 router.post('/task', resolver(TaskController.store));
-router.put('/task/:id', resolver(TaskController.update));
-router.delete('/task/:id', resolver(TaskController.delete));
 
-router.get('/users', resolver(UserController.index));
-router.put('/user/:id', resolver(UserController.update));
-router.delete('/user/:id', resolver(UserController.delete))
+
+
+router.use(authenticationMiddleware);
 
 router.get('/user/:id', resolver(UserController.show));
-router.use(authenticationMiddleware);
+router.delete('/user/:id', resolver(UserController.delete))
+router.put('/user/:id', resolver(UserController.update));
+
 router.get('/task/:id', resolver(TaskController.show));
+router.put('/task/:id', resolver(TaskController.update));
+router.delete('/task/:id', resolver(TaskController.delete));
 
 router.use(httpErrorHandler)
 
