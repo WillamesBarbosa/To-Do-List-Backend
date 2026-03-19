c53bb35bbffb74666b110af7e456d34cc56a2df4
WillamesBarbosa <willamessilva1999@gmail.com>
2026-03-19 07:22:41 -0300

    test(loginController): Import the loginService and mock RepositoryRefreshToken to avoid calling Redis directly.

--- tests/controllers/loginController/unit/loginController.test.js
+++ tests/controllers/loginController/unit/loginController.test.js
@@ -1,10 +1,16 @@
+jest.mock('../../../../src/app/repositories/RefreshTokenRepository/RefreshTokenRepository', () => ({
+    findByUserId: jest.fn(),
+    delete: jest.fn(),
+    save: jest.fn()
+}));
+
 jest.mock('../../../../src/app/services/loginService/loginService', () => jest.fn());
 jest.mock('../../../../src/app/utils/validators/verifyParams/verifyParams', ()=> jest.fn())
 jest.mock('../../../../src/app/utils/validators/isValidEmail/isValidEmail', ()=> jest.fn())
 
-const loginService = require('../../../../src/app/services/loginService/loginService');
 
 const loginController = require('../../../../src/app/controllers/LoginController/loginController');
+const loginService = require('../../../../src/app/services/loginService/loginService');
 const responsesHTTP = require('../../../../src/app/utils/helpers/responsesHTTPS');
 
 let response;
@@ -18,7 +24,8 @@ describe('Test LoginController', ()=>{
     test('Should return 200 and token', async()=>{
         loginService.mockResolvedValue({
             isValid: true,
-            token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxMjMiLCJlbWFpbCI6InRlc3RAZXhhbXBsZS5jb20iLCJpYXQiOjE2OTk5OTk5OTl9.mockSignature'    
+            token: 'fake-access-jwt',
+            refreshToken: 'fake-refresh-jwt'
         })
 
         await loginController.login({body: {email: 'email@email.com', password: '123'}}, response);
