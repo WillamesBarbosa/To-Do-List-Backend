cd4990179c0ca3eb7bbfaf7f5627265b9a11c8ab
WillamesBarbosa <willamessilva1999@gmail.com>
2026-03-27 19:55:47 -0300

    fix(config): fix redis auth and node env for production
    - use REDIS_URL for railway redis connection with authentication
    - fallback to host/port for local development

--- src/database/config/config-redis.js
+++ src/database/config/config-redis.js
@@ -1,6 +1,10 @@
 const Redis = require('ioredis');
+require('dotenv').config();
 
-const redis = new Redis({
+const redis = process.env.REDIS_URL
+? new Redis(process.env.REDIS_URL)
+:
+new Redis({
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
 });
